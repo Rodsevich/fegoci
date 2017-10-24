c28cb0ac4dd651c8fd34bdb1829f745cf5e267f8
Rodsevich <nicorodsevich@gmail.com>
2017-10-24 11:56:41 -0300

    comienzo con angular app

---
+++ lib/src/angular/componentes/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:angular/angular.dart';
+
+@Component(
+  selector: 'app',
+  templateUrl: 'app.html',
+  styleUrls: const ['app.css'])
+class App implements OnInit {
+
+  App();
+
+  @override
+  void ngOnInit() {}
+
+}
