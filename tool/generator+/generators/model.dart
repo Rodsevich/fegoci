509bbdeab043417b36ee05cca00d23d6aa37800c
Rodsevich <nicorodsevich@gmail.com>
2017-11-03 18:09:30 -0300

    model generator

--- tool/generator+/generators/model.dart
+++ tool/generator+/generators/model.dart
@@ -6,11 +6,14 @@ class ModelGenerator extends Generator {
   String get description =>
       "crea un modelo usable entre backend-frontend indistintamente";
 
-  OverridingPolicy get overridePolicy => OverridingPolicy.NEVER;
+  OverridingPolicy get overridePolicy => OverridingPolicy.ALWAYS;
   List<Dependency> get alwaysNeededDependencies => null;
   Map get startingVariables => null;
 
-  Future execution() {
-    generateTemplateDir("tool/generator+/templates/model");
+  Future execution() async {
+    askForInput("Model name", ".*").then((String name) {
+      variablesResolver["name"] = name;
+    });
+    await generateTemplateDir("tool/generator+/templates/model");
   }
 }
